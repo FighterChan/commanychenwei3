79871d470e4712c12ec1fd1f569366b607db1a73
chennono <920847476@qq.com>
2018-11-14 00:29:54 +0800

    temp

--- main.h
+++ main.h
@@ -3,6 +3,10 @@
 #define _MAIN_H_
 
 #include <stdio.h>
+#include "arp.h"
+#include "mac.h"
+#include "adj.h"
+#include "list.h"
 
 enum {
 	FALSE, TRUE = !FALSE
@@ -14,6 +18,8 @@ enum {
 
 #define APP_SUCC (0)
 #define APP_ERR  (-1)
+#define CMD_MAX (9)
+
 
 #define DEFINE_PARAM(x)\
 do { \
@@ -23,7 +29,15 @@ do { \
 	} \
 }while(0)
 
-#define CMD_MAX (9)
+
+struct table_node {
+	struct arp_table *parp;
+	struct mac_table *pmac;
+	struct adj_table *padj;
+	struct list_head *arp_pos, *arp_next;
+	struct list_head *mac_pos, *mac_next;
+	struct list_head *adj_pos, *adj_next;
+};
 
 /* body of main.h */
 #endif /* _MAIN_H_ */
